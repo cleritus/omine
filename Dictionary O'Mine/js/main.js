aab40eb142f7b5182543be8af2f629b8cad58749
cleritus <lukasz.stank@gmail.com>
2019-09-17 11:05:00 +0200

    js: plug hide and show buttons

--- Dictionary O'Mine/js/main.js
+++ Dictionary O'Mine/js/main.js
@@ -1,15 +1,22 @@
 const inputWord = document.querySelector('.word-input');
-const inputTranslation = document.querySelector('.translation-input');
+const inputTranslate = document.querySelector('.translate-input');
 const inputExample = document.querySelector('.example-input');
 const addBtn = document.querySelector('.add-btn');
 const tbody = document.querySelector('.tbody');
 
+const wordArray = [];
+const translateArray = [];
+const exampleArray = [];
 
+
+const wordBtn = document.querySelector('.word-btn');
+const translateBtn = document.querySelector('.translate-btn');
+const exampleBtn = document.querySelector('.example-btn');
 
 
 const addTerm = (e) => {
   e.preventDefault();
-  if ((inputWord.value === '') || (inputTranslation.value === '') || (inputExample.value === '')) return;
+  // if ((inputWord.value === '') || (inputTranslate.value === '') || (inputExample.value === '')) return;
 
   const tr = document.createElement('tr');
   tbody.appendChild(tr);
@@ -18,19 +25,47 @@ const addTerm = (e) => {
   tr.appendChild(tdWord);
   tdWord.textContent = inputWord.value;
 
-  const tdTranslation = document.createElement('td');
-  tr.appendChild(tdTranslation);
-  tdTranslation.textContent = inputTranslation.value;
+  const tdTranslate = document.createElement('td');
+  tr.appendChild(tdTranslate);
+  tdTranslate.textContent = inputTranslate.value;
 
   const tdExample = document.createElement('td');
   tr.appendChild(tdExample);
   tdExample.textContent = inputExample.value;
 
+  const pushToArray = () => {
+    wordArray.push(tdWord);
+    translateArray.push(tdTranslate);
+    exampleArray.push(tdExample);
+  }
+
+  pushToArray();
 
   inputWord.value = '';
-  inputTranslation.value = '';
+  inputTranslate.value = '';
   inputExample.value = '';
 
 }
 
+//After click show or hide columns
+
+const hideWords = () => {
+  wordArray.forEach(item => item.classList.toggle('hidden'));
+}
+
+const hideTranslate = () => {
+  translateArray.forEach(item => item.classList.toggle('hidden'))
+}
+
+const hideExample = () => {
+  exampleArray.forEach(item => item.classList.toggle('hidden'))
+}
+
+// Change words in a frame
+
+
+
 addBtn.addEventListener('click', addTerm);
+wordBtn.addEventListener('click', hideWords);
+translateBtn.addEventListener('click', hideTranslate);
+exampleBtn.addEventListener('click', hideExample);
